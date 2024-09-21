a772ce33a272e79eba5aceeeec786a9b623de927
Felipe Lunkes <felipe.lunkes@orsegups.com.br>
2024-09-21 02:55:31 -0300

    Initial page

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
